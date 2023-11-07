9d2e822eabd1b07cbaf37097c75b9f90b9c530d5
v-duma <139060657+v-duma@users.noreply.github.com>
2023-11-07 23:25:55 +0200

    task 1 done

---
+++ js/01-gallery.js
@@ -0,0 +1,58 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const ul = document.querySelector(".gallery");
+let instance;
+
+function createGalleryItems() {
+  const items = [];
+
+  for (let i = 0; i < galleryItems.length; i++) {
+    const li = document.createElement("li");
+    li.classList.add("gallery__item");
+
+    const a = document.createElement("a");
+    a.classList.add("gallery__link");
+    a.href = galleryItems[i].original;
+
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = galleryItems[i].preview;
+    img.dataset.source = galleryItems[i].original;
+    img.alt = galleryItems[i].description;
+
+    a.appendChild(img);
+    li.appendChild(a);
+    items.push(li);
+  }
+
+  ul.append(...items);
+}
+
+const selectLi = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImgSource = event.target.dataset.source;
+  const selectedImgAlt = event.target.alt;
+
+  instance = basicLightbox.create(
+    `<img src="${selectedImgSource}" alt="${selectedImgAlt}">`
+  );
+
+  instance.show();
+  ul.addEventListener("keydown", closeImg);
+};
+
+const closeImg = (event) => {
+  if (event.code === "Space") {
+    instance.close();
+    ul.removeEventListener("keydown", closeImg);
+  }
+};
+
+createGalleryItems();
+ul.addEventListener("click", selectLi);
+ul.addEventListener("keydown", closeImg);
